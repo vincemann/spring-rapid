09a37815e57409f3050bc76e5ad0b24b665d0fdc
vincemann <v.conrad98@web.de>
2020-03-11 20:21:12 +0100

    make config conditionalOnMissingBean when appropriate

--- library/src/main/java/io/github/vincemann/generic/crud/lib/config/JacksonConfig.java
+++ library/src/main/java/io/github/vincemann/generic/crud/lib/config/JacksonConfig.java
@@ -3,6 +3,7 @@ package io.github.vincemann.generic.crud.lib.config;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 @Profile("web")
 public class JacksonConfig {
 
+    @ConditionalOnMissingBean
     @Bean
     public ObjectMapper objectMapper(){
         ObjectMapper mapper= new ObjectMapper();
