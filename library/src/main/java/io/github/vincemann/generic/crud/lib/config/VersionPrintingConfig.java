0c3b18b1ceaac691127bb1c87dafad6b18e728fc
vincemann <v.conrad98@web.de>
2020-03-25 15:06:43 +0100

    change version info

--- library/src/main/java/io/github/vincemann/generic/crud/lib/config/VersionPrintingConfig.java
+++ library/src/main/java/io/github/vincemann/generic/crud/lib/config/VersionPrintingConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @Slf4j
 public class VersionPrintingConfig {
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     public VersionPrintingConfig() {
         log.info("Using Spring-Crud-Lib Version: " + VERSION);
