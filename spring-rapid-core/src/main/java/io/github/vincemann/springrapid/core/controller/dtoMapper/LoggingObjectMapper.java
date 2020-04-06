645b18b84779a0e6aaf15a72a4b85c5425e95b33
vincemann <v.conrad98@web.de>
2020-04-06 20:49:34 +0200

    update to spring 2.2.1

--- spring-rapid-core/src/main/java/io/github/vincemann/springrapid/core/controller/dtoMapper/LoggingObjectMapper.java
+++ spring-rapid-core/src/main/java/io/github/vincemann/springrapid/core/controller/dtoMapper/LoggingObjectMapper.java
@@ -14,19 +14,19 @@ public class LoggingObjectMapper extends ObjectMapper {
 
     @LogInteraction
     @Override
-    public <T> T readValue(String content, Class<T> valueType) throws IOException, JsonParseException, JsonMappingException {
+    public <T> T readValue(String content, Class<T> valueType) throws JsonProcessingException {
         return super.readValue(content, valueType);
     }
 
     @LogInteraction
     @Override
-    public <T> T readValue(String content, TypeReference valueTypeRef) throws IOException, JsonParseException, JsonMappingException {
+    public <T> T readValue(String content, TypeReference<T> valueTypeRef) throws JsonProcessingException, JsonMappingException {
         return super.readValue(content, valueTypeRef);
     }
 
     @LogInteraction
     @Override
-    public <T> T readValue(String content, JavaType valueType) throws IOException, JsonParseException, JsonMappingException {
+    public <T> T readValue(String content, JavaType valueType) throws JsonProcessingException {
         return super.readValue(content, valueType);
     }
 
