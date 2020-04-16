656c50cb01b960c71bcb95b5b02c2c9929efdf7b
vincemann <v.conrad98@web.de>
2020-04-16 18:11:28 +0200

    small fix

--- spring-rapid-acl/src/main/java/io/github/vincemann/springrapid/acl/Permission.java
+++
@@ -1,8 +0,0 @@
-package io.github.vincemann.springrapid.acl;
-
-public interface Permission {
-    static String READ = "READ";
-    static String WRITE = "WRITE";
-    static String DELETE = "DELETE";
-    static String CREATE = "CREATE";
-}
