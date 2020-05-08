7b69122667f405a10aee05fe864968a4069c1c45
vincemann <v.conrad98@web.de>
2020-05-08 15:50:15 +0200

    add parentAware Rapid controller to core package

---
+++ spring-rapid-core/src/main/java/io/github/vincemann/springrapid/core/controller/rapid/parentAware/ParentAwareDtoEndpoint.java
@@ -0,0 +1,5 @@
+package io.github.vincemann.springrapid.core.controller.rapid.parentAware;
+
+public class ParentAwareDtoEndpoint {
+    public static final String FIND_ALL_OF_PARENT = "findAllOfParent";
+}
