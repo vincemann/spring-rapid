d37f989d78bfb2b14e9c348e337a001913574537
vincemann <v.conrad98@web.de>
2020-06-01 13:41:26 +0200

    working on config model

--- spring-rapid-compare/src/main/java/io/github/vincemann/springrapid/compare/template/AdditionalActorConfigurer.java
+++
@@ -1,6 +0,0 @@
-package io.github.vincemann.springrapid.compare.template;
-
-
-public interface AdditionalActorConfigurer extends ActorConfigurer{
-    public PropertyConfigurer properties();
-}
--- spring-rapid-compare/src/main/java/io/github/vincemann/springrapid/compare/template/SelectingPropertyConfigurer.java
+++
@@ -1,9 +0,0 @@
-package io.github.vincemann.springrapid.compare.template;
-
-import com.github.hervian.reflection.Types;
-
-public interface SelectingPropertyConfigurer {
-    AdditionalSelectingPropertyConfigurer include(Types.Supplier<?> getter);
-    AdditionalSelectingPropertyConfigurer include(String propertyName);
-
-}
