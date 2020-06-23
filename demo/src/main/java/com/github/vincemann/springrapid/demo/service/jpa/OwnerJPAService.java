7d1756c60ab0c17c35e3d9a10a4a43921155e900
vincemann <v.conrad98@web.de>
2020-06-23 16:02:32 +0200

    again

--- demo/src/main/java/com/github/vincemann/springrapid/demo/service/jpa/OwnerJPAService.java
+++ demo/src/main/java/com/github/vincemann/springrapid/demo/service/jpa/OwnerJPAService.java
@@ -8,6 +8,7 @@ import com.github.vincemann.springrapid.core.service.jpa.JPACrudService;
 import com.github.vincemann.aoplog.annotation.AopLoggable;
 
 import com.github.vincemann.aoplog.annotation.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
