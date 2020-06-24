50990743d5e7e8fa8f703e9f9c5bffe8f66f739b
vincemann <v.conrad98@web.de>
2020-06-24 13:55:55 +0200

    fix entity relation ship tests

--- entity-relationship/src/test/java/com/github/vincemann/springrapid/entityrelationship/controller/uniDir/testEntities/UniDirEntityChildsParent.java
+++ entity-relationship/src/test/java/com/github/vincemann/springrapid/entityrelationship/controller/uniDir/testEntities/UniDirEntityChildsParent.java
@@ -1,6 +1,7 @@
 package com.github.vincemann.springrapid.entityrelationship.controller.uniDir.testEntities;
 
 import com.github.vincemann.springrapid.core.model.IdentifiableEntityImpl;
+import com.github.vincemann.springrapid.entityrelationship.model.parent.UniDirParent;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,5 +9,5 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class UniDirEntityChildsParent extends IdentifiableEntityImpl<Long> {
+public class UniDirEntityChildsParent extends IdentifiableEntityImpl<Long> implements UniDirParent {
 }
