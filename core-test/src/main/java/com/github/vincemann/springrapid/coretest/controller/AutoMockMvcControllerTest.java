e43dc70f57a98b98e0e3e9760bc4f311fc068c5f
vincemann <v.conrad98@web.de>
2020-11-21 11:35:16 +0100

    work on #72.

--- core-test/src/main/java/com/github/vincemann/springrapid/coretest/controller/AutoMockMvcControllerTest.java
+++ core-test/src/main/java/com/github/vincemann/springrapid/coretest/controller/AutoMockMvcControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
