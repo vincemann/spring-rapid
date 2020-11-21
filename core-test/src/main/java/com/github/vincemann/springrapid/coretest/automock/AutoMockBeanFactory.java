b885ff4f819d6992fba1ef87c73850e5ec3d75f6
vincemann <v.conrad98@web.de>
2020-11-21 11:35:16 +0100

    work on #72.

--- core-test/src/main/java/com/github/vincemann/springrapid/coretest/automock/AutoMockBeanFactory.java
+++ core-test/src/main/java/com/github/vincemann/springrapid/coretest/automock/AutoMockBeanFactory.java
@@ -7,16 +7,26 @@ import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Service;
 
 import java.beans.Introspector;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.mock;
 
+/**
+ * Automatically mocks all beans annotated with {@link ServiceComponent} or {@link Service}.
+ * Used for controller testing -> use of @{@link org.springframework.boot.test.mock.mockito.MockBean} for service beans becomes needless.
+ * Supports context caching because all service beans are mocked (otherwise for each diff subset of manually mocked beans with @{@link org.springframework.boot.test.mock.mockito.MockBean}
+ * a new context gets created -> context cant be cached -> slow)
+ */
 @Slf4j
 public class AutoMockBeanFactory extends DefaultListableBeanFactory {
+
+    private HashMap<String,Object> mockedBeans = new HashMap<>();
 
     @Override
     protected Map<String, Object> findAutowireCandidates(final String beanName, final Class<?> requiredType, final DependencyDescriptor descriptor) {
@@ -33,11 +43,17 @@ public class AutoMockBeanFactory extends DefaultListableBeanFactory {
         }
         if (autowireCandidates.isEmpty()) {
             //todo @Repository auch automocken?
+            //todo does this support @Qualifiers ? -> create diff mocks for @Autowired service and  @Secured @Autowired service ?
             if(requiredType.isAnnotationPresent(ServiceComponent.class)
                     || requiredType.isAnnotationPresent(Service.class)) {
-                final Object mock = mock(requiredType);
-                log.info("Automocking bean with name " + mockBeanName + "of type: " + requiredType.getSimpleName());
+                // prevent double mock creation of same singleton bean requested multiple times
+                Object mock = mockedBeans.get(mockBeanName);
+                if (mock==null){
+                    log.info("Automocking bean with name " + mockBeanName + "of type: " + requiredType.getSimpleName());
+                    mock =  mock(requiredType);
+                }
                 autowireCandidates.put(mockBeanName, mock);
+                mockedBeans.put(mockBeanName,mock);
                 this.addSingleton(mockBeanName, mock);
             }
         }
