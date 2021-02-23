d48c24947901729794a41a7d10b80d0fb4b5c0fe
vincemann <v.conrad98@web.de>
2021-02-23 16:09:35 +0100

    Fix Autoconfigure issue for auth-tests login templates

--- auth-demo/src/main/resources/create-lemon-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-homeDir=`cd ~ && pwd`
-rapidDir=$homeDir/.rapid
-mkdir -p $rapidDir/mysql/data
-mkdir -p $rapidDir/mysql/conf.d
-cp db/init-database.sql $rapidDir/
-sudo docker run --detach --name=lemon-mysql --env="MYSQL_ROOT_PASSWORD=mypassword" --env="MYSQL_DATABASE=lemonDb" --publish 6603:3306 --volume=$rapidDir/mysql/conf.d:/etc/mysql/conf.d --volume=$rapidDir/mysql/data:/var/lib/mysql --volume=$rapidDir:/docker-entrypoint-initdb.d mysql:8.0
-
--- auth-test/src/main/java/com/github/vincemann/springrapid/authtest/AuthMockLoginTemplate.java
+++ auth-test/src/main/java/com/github/vincemann/springrapid/authtest/AuthMockLoginTemplate.java
@@ -2,6 +2,7 @@ package com.github.vincemann.springrapid.authtest;
 
 import com.github.vincemann.springrapid.auth.domain.AbstractUser;
 import com.github.vincemann.springrapid.auth.security.AuthenticatedPrincipalFactory;
+import com.github.vincemann.springrapid.core.security.RapidAuthenticatedPrincipal;
 import com.github.vincemann.springrapid.coretest.login.MockLoginTemplate;
 import com.github.vincemann.springrapid.coretest.login.RapidMockLoginTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class AuthMockLoginTemplate implements MockLoginTemplate<AbstractUser> {
 
     private AuthenticatedPrincipalFactory authenticatedPrincipalFactory;
-    private RapidMockLoginTemplate mockLoginTemplate;
+    private MockLoginTemplate<RapidAuthenticatedPrincipal> mockLoginTemplate;
 
     @Override
     public void login(AbstractUser principal) {
@@ -22,7 +23,7 @@ public class AuthMockLoginTemplate implements MockLoginTemplate<AbstractUser> {
         this.authenticatedPrincipalFactory = authenticatedPrincipalFactory;
     }
     @Autowired
-    public void injectMockAuthLoginTemplate(RapidMockLoginTemplate mockLoginTemplate) {
+    public void injectMockLoginTemplate(MockLoginTemplate<RapidAuthenticatedPrincipal> mockLoginTemplate) {
         this.mockLoginTemplate = mockLoginTemplate;
     }
 }
