49154aa37b6b25703cd2bf518f913344a775b515
vincemann <v.conrad98@web.de>
2021-04-18 18:49:10 +0200

    Write Vet Tests for #135.

--- core-demo/src/test/java/com/github/vincemann/springrapid/coredemo/service/jpa/VerServiceIntegrationTest.java
+++ core-demo/src/test/java/com/github/vincemann/springrapid/coredemo/service/jpa/VerServiceIntegrationTest.java
@@ -9,34 +9,35 @@ import com.github.vincemann.springrapid.coredemo.service.SpecialtyService;
 import com.github.vincemann.springrapid.coredemo.service.VetService;
 import com.github.vincemann.springrapid.coretest.service.CrudServiceIntegrationTest;
 import com.github.vincemann.springrapid.coretest.util.RapidTestUtil;
+import org.apache.commons.beanutils.BeanUtilsBean;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 import static com.github.vincemann.ezcompare.Comparator.compare;
+import static com.github.vincemann.springrapid.coretest.service.ExceptionMatchers.noException;
+import static com.github.vincemann.springrapid.coretest.service.ExistenceMatchers.notPresentInDatabase;
 import static com.github.vincemann.springrapid.coretest.service.PropertyMatchers.propertyAssert;
-import static com.github.vincemann.springrapid.coretest.service.request.CrudServiceRequestBuilders.save;
+import static com.github.vincemann.springrapid.coretest.service.request.CrudServiceRequestBuilders.*;
 import static com.github.vincemann.springrapid.coretest.service.resolve.EntityPlaceholder.DB_ENTITY;
 import static com.github.vincemann.springrapid.coretest.service.resolve.EntityPlaceholder.SERVICE_RETURNED_ENTITY;
 
-public class VerServiceIntegrationTest extends CrudServiceIntegrationTest<VetService,Vet,Long> {
-
-    //Types
-    Vet VetType = new Vet();
+public class VerServiceIntegrationTest extends CrudServiceIntegrationTest<VetService, Vet, Long> {
 
     protected static final String MEIER = "Meier";
     protected static final String KAHN = "Kahn";
-
     protected static final String DENTISM = "Dentism";
     protected static final String GASTRO = "Gastro";
     protected static final String HEART = "Heart";
-
+    //Types
+    Vet VetType = new Vet();
     Vet meier;
     Vet kahn;
 
@@ -63,11 +64,11 @@ public class VerServiceIntegrationTest extends CrudServiceIntegrationTest<VetSer
                 .build();
 
         heart = Specialty.builder()
-                .description(GASTRO)
+                .description(HEART)
                 .build();
 
         gastro = Specialty.builder()
-                .description(HEART)
+                .description(GASTRO)
                 .build();
 
         meier = Vet.builder()
@@ -85,40 +86,189 @@ public class VerServiceIntegrationTest extends CrudServiceIntegrationTest<VetSer
 
     @Test
     public void canSaveVetWithoutSpecialty() {
-                test(save(meier))
-                        .andExpect(() -> compare(meier)
-                                // resolve db entity makes sure entity is actually saved in repo
-                                .with(resolve(DB_ENTITY))
-                                .properties()
-                                .all()
-                                .ignore(VetType::getId)
-                                .assertEqual())
-                        // is implicitly tested by next statement, just want to make it explicit
-                        .andExpect(()-> propertyAssert(resolve(SERVICE_RETURNED_ENTITY))
-                                .assertEmpty(VetType::getSpecialties)
-                        )
-                        .andExpect(() -> compare(meier).with(resolve(SERVICE_RETURNED_ENTITY))
-                                .properties()
-                                .all()
-                                .ignore(VetType::getId)
-                                .assertEqual());
+        test(save(meier))
+                .andExpect(() -> compare(meier)
+                        // resolve db entity makes sure entity is actually saved in repo
+                        .with(resolve(DB_ENTITY))
+                        .properties()
+                        .all()
+                        .ignore(VetType::getId)
+                        .assertEqual())
+                // is implicitly tested by next statement, just want to make it explicit
+                .andExpect(() -> propertyAssert(resolve(SERVICE_RETURNED_ENTITY))
+                        .assertEmpty(VetType::getSpecialties)
+                )
+                .andExpect(() -> compare(meier).with(resolve(SERVICE_RETURNED_ENTITY))
+                        .properties()
+                        .all()
+                        .ignore(VetType::getId)
+                        .assertEqual());
     }
 
     @Test
-    public void canSaveVetWithSpecialties() throws BadEntityException {
+    public void canSaveVet_getLinkedToSpecialties() throws BadEntityException {
         Specialty savedDentism = specialtyService.save(dentism);
         Specialty savedGastro = specialtyService.save(gastro);
 
-        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism,savedGastro)));
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
 
         test(save(meier));
 
-        assertVetHasSpecialties(MEIER,DENTISM,GASTRO);
-        assertSpecialtyHasVets(GASTRO,MEIER);
-        assertSpecialtyHasVets(DENTISM,MEIER);
+        assertVetHasSpecialties(MEIER, DENTISM, GASTRO);
+        assertSpecialtyHasVets(GASTRO, MEIER);
+        assertSpecialtyHasVets(DENTISM, MEIER);
     }
 
-    private void assertVetHasSpecialties(String vetName, String... descriptions){
+    @Test
+    public void canFindVetWithMultipleSpecialties() throws BadEntityException {
+        Specialty savedDentism = specialtyService.save(dentism);
+        Specialty savedGastro = specialtyService.save(gastro);
+
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
+
+        Vet savedMeier = getServiceUnderTest().save(meier);
+        test(findById(savedMeier.getId()))
+                .andExpect(() -> compare(savedMeier).with(resolve(DB_ENTITY))
+                        .properties()
+                        .all()
+                        .assertEqual());
+
+        assertVetHasSpecialties(MEIER, DENTISM, GASTRO);
+        assertSpecialtyHasVets(GASTRO, MEIER);
+        assertSpecialtyHasVets(DENTISM, MEIER);
+    }
+
+    // specialty that is already linked to meier, will be also linked to kahn after save(kahn)
+    @Test
+    public void canAddAnotherVetToSpecialty_viaSave() throws BadEntityException {
+        Specialty savedDentism = specialtyService.save(dentism);
+        Specialty savedGastro = specialtyService.save(gastro);
+
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
+
+        Vet savedMeier = getServiceUnderTest().save(meier);
+        kahn.setSpecialties(new HashSet<>(Lists.newArrayList(savedGastro)));
+        test(save(kahn));
+
+        assertVetHasSpecialties(MEIER, DENTISM, GASTRO);
+        assertVetHasSpecialties(KAHN, GASTRO);
+        assertSpecialtyHasVets(GASTRO, MEIER, KAHN);
+        assertSpecialtyHasVets(DENTISM, MEIER);
+    }
+
+    @Test
+    public void canUnlinkSpecialtyFromVet_viaPartialUpdate() throws BadEntityException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Specialty savedDentism = specialtyService.save(dentism);
+        Specialty savedGastro = specialtyService.save(gastro);
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
+        Vet savedMeier = getServiceUnderTest().save(meier);
+        kahn.setSpecialties(new HashSet<>(Lists.newArrayList(savedGastro)));
+        Vet savedKahn = getServiceUnderTest().save(kahn);
+
+        // remove dentism from meier
+        Vet meierUpdate = Vet.builder()
+                .specialties(new HashSet<>(Lists.newArrayList(savedGastro)))
+                .build();
+        meierUpdate.setId(savedMeier.getId());
+        test(partialUpdate(meierUpdate))
+                // check that it really was only partial update
+                .andExpect(() -> propertyAssert(resolve(DB_ENTITY))
+                        .assertEquals(VetType::getLastName, MEIER));
+
+        assertVetHasSpecialties(MEIER, GASTRO);
+        assertVetHasSpecialties(KAHN, GASTRO);
+        assertSpecialtyHasVets(GASTRO, MEIER, KAHN);
+        assertSpecialtyHasVets(DENTISM);
+    }
+
+    @Test
+    public void canUnlinkSpecialtyFromVet_viaFullUpdate() throws BadEntityException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        Specialty savedDentism = specialtyService.save(dentism);
+        Specialty savedGastro = specialtyService.save(gastro);
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
+        Vet savedMeier = getServiceUnderTest().save(meier);
+        kahn.setSpecialties(new HashSet<>(Lists.newArrayList(savedGastro)));
+        Vet savedKahn = getServiceUnderTest().save(kahn);
+
+        // remove dentism from meier
+        Vet meierUpdate = (Vet) BeanUtilsBean.getInstance().cloneBean(savedMeier);
+        meierUpdate.setSpecialties(new HashSet<>(Lists.newArrayList(savedGastro)));
+        test(update(meierUpdate));
+
+        assertVetHasSpecialties(MEIER, GASTRO);
+        assertVetHasSpecialties(KAHN, GASTRO);
+        assertSpecialtyHasVets(GASTRO, MEIER, KAHN);
+        assertSpecialtyHasVets(DENTISM);
+    }
+
+    @Test
+    public void canLinkAnotherSpecialtyToVet_viaPartialUpdate() throws BadEntityException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        // kahn -> gastro
+        // meier -> gastro, dentism
+        Specialty savedDentism = specialtyService.save(dentism);
+        Specialty savedGastro = specialtyService.save(gastro);
+        Specialty savedHeart = specialtyService.save(heart);
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
+        Vet savedMeier = getServiceUnderTest().save(meier);
+        kahn.setSpecialties(new HashSet<>(Lists.newArrayList(savedGastro)));
+        Vet savedKahn = getServiceUnderTest().save(kahn);
+
+        // add heart to kahn
+        Vet kahnUpdate = Vet.builder()
+                .specialties(new HashSet<>(Lists.newArrayList(savedGastro, savedHeart)))
+                .build();
+        kahnUpdate.setId(savedKahn.getId());
+        test(partialUpdate(kahnUpdate));
+
+        assertVetHasSpecialties(MEIER, GASTRO, DENTISM);
+        assertVetHasSpecialties(KAHN, GASTRO, HEART);
+        assertSpecialtyHasVets(GASTRO, MEIER, KAHN);
+        assertSpecialtyHasVets(DENTISM, MEIER);
+        assertSpecialtyHasVets(HEART, KAHN);
+    }
+
+    @Test
+    public void canLinkAnotherSpecialtyToVet_viaFullUpdate() throws BadEntityException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        // kahn -> gastro
+        // meier -> gastro, dentism
+        Specialty savedDentism = specialtyService.save(dentism);
+        Specialty savedGastro = specialtyService.save(gastro);
+        Specialty savedHeart = specialtyService.save(heart);
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
+        Vet savedMeier = getServiceUnderTest().save(meier);
+        kahn.setSpecialties(new HashSet<>(Lists.newArrayList(savedGastro)));
+        Vet savedKahn = getServiceUnderTest().save(kahn);
+
+        // add heart to kahn
+        Vet kahnUpdate = (Vet) BeanUtilsBean.getInstance().cloneBean(savedKahn);
+        kahnUpdate.setSpecialties(new HashSet<>(Lists.newArrayList(savedGastro, savedHeart)));
+        test(update(kahnUpdate));
+
+        assertVetHasSpecialties(MEIER, GASTRO, DENTISM);
+        assertVetHasSpecialties(KAHN, GASTRO, HEART);
+        assertSpecialtyHasVets(GASTRO, MEIER, KAHN);
+        assertSpecialtyHasVets(DENTISM, MEIER);
+        assertSpecialtyHasVets(HEART, KAHN);
+    }
+
+
+    @Test
+    public void canDeleteVet_getUnlinkedFromSpecialties() throws BadEntityException {
+        Specialty savedDentism = specialtyService.save(dentism);
+        Specialty savedGastro = specialtyService.save(gastro);
+
+        meier.setSpecialties(new HashSet<>(Lists.newArrayList(savedDentism, savedGastro)));
+
+        Vet savedMeier = getServiceUnderTest().save(meier);
+        test(deleteById(savedMeier.getId()))
+                .andExpect(noException())
+                .andExpect(notPresentInDatabase(savedMeier.getId()));
+
+        assertSpecialtyHasVets(GASTRO);
+        assertSpecialtyHasVets(DENTISM);
+    }
+
+    private void assertVetHasSpecialties(String vetName, String... descriptions) {
         Optional<Vet> vetOptional = vetRepository.findByLastName(vetName);
         Assertions.assertTrue(vetOptional.isPresent());
         Vet vet = vetOptional.get();
@@ -129,10 +279,10 @@ public class VerServiceIntegrationTest extends CrudServiceIntegrationTest<VetSer
             Assertions.assertTrue(optionalSpecialty.isPresent());
             specialties.add(optionalSpecialty.get());
         }
-        Assertions.assertEquals(specialties,vet.getSpecialties());
+        Assertions.assertEquals(specialties, vet.getSpecialties());
     }
 
-    private void assertSpecialtyHasVets(String description, String... vetNames){
+    private void assertSpecialtyHasVets(String description, String... vetNames) {
         Optional<Specialty> optionalSpecialty = specialtyRepository.findByDescription(description);
         Assertions.assertTrue(optionalSpecialty.isPresent());
         Specialty specialty = optionalSpecialty.get();
