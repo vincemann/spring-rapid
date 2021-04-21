cd24d91dc3e41d9e63d73cc39353063c428df2b5
vincemann <v.conrad98@web.de>
2021-04-21 17:34:58 +0200

    enhance toString methods and prepare for #142.

--- core-demo/src/main/java/com/github/vincemann/springrapid/coredemo/model/Visit.java
+++ core-demo/src/main/java/com/github/vincemann/springrapid/coredemo/model/Visit.java
@@ -1,34 +1,56 @@
 package com.github.vincemann.springrapid.coredemo.model;
 
+import com.github.vincemann.springrapid.entityrelationship.model.child.annotation.UniDirChildCollection;
 import com.github.vincemann.springrapid.entityrelationship.model.child.annotation.UniDirChildEntity;
 import com.github.vincemann.springrapid.entityrelationship.model.parent.UniDirParent;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 import com.github.vincemann.springrapid.core.model.IdentifiableEntityImpl;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@ToString
 public class Visit extends IdentifiableEntityImpl<Long> implements UniDirParent {
 
-    @ManyToOne
+
+    @OneToMany
     @JoinColumn(name = "pet_id")
+    @UniDirChildCollection(Pet.class)
+    private Set<Pet> pets = new HashSet<>();
+
+    @OneToOne
     @UniDirChildEntity
-    private Pet pet;
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @OneToOne
+    @UniDirChildEntity
+    @JoinColumn(name = "vet_id")
+    private Vet vet;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "date")
     private LocalDate date;
 
-    @Column(name = "description")
-    private String description;
+    @Column(name = "reason")
+    private String reason;
+
+    @Override
+    public String toString() {
+        return "Visit{" +
+                "pets=" + Arrays.toString(pets.stream().map(Pet::getName).toArray())  +
+                ", owner=" + (owner==null? "null": owner.getLastName()) +
+                ", vet=" + (vet==null? "null": vet.getLastName()) +
+                ", date=" + date +
+                ", reason='" + reason + '\'' +
+                '}';
+    }
 }
