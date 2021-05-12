3894ccac14d9a764691e4757bee7019238ee8cd9
vincemann <v.conrad98@web.de>
2021-05-12 19:50:34 +0200

    Change email test refactoring finished, #152.

--- auth-tests/src/main/java/com/github/vincemann/springrapid/authtests/ChangeEmailTest.java
+++ auth-tests/src/main/java/com/github/vincemann/springrapid/authtests/ChangeEmailTest.java
@@ -25,43 +25,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class ChangeEmailTest extends AbstractRapidAuthIntegrationTest {
-	
-	private static final String NEW_EMAIL = "new.email@example.com";
-
 
 	@Autowired
 	private JweTokenService jweTokenService;
-//
-//	@Autowired
-//	private AbstractUserRepository userRepository;
-
-//	@Override
-//	protected void createTestUsers() throws Exception{
-//		super.createTestUsers();
-//		// todo create method for requesting email change in testTemplate
-//		AbstractUser<Long> user = getUserService().findById(getUser().getId()).get();
-//		user.setNewEmail(NEW_EMAIL);
-//		userRepository.save(user);
-//	}
-
-//	@BeforeEach
-//	protected void setupEmailCode() throws Exception {
-//		changeEmailCode = jweTokenService.createToken(
-//				RapidJwt.create(
-//				AbstractUserService.CHANGE_EMAIL_AUDIENCE,
-//				Long.toString(getUser().getId()),
-//				600000L,
-//				MapUtils.mapOf("newEmail", NEW_EMAIL)));
-//	}
-
-//	protected String createChangeEmailToken(AbstractUser targetUser, String newEmail, Long expiration){
-//		return jweTokenService.createToken(
-//				RapidJwt.create(
-//						AbstractUserService.CHANGE_EMAIL_AUDIENCE,
-//						targetUser.getId().toString(),
-//						expiration,
-//						MapUtils.mapOf("newEmail", newEmail)));
-//	}
 
 
 	//works solo but token is obsolete when run in group
@@ -162,64 +128,70 @@ public class ChangeEmailTest extends AbstractRapidAuthIntegrationTest {
 //     * Providing an obsolete changeEmailCode shouldn't work.
 //     */
 //    //todo sometimes 401 sometimes 403
-//	@Test
-//	public void obsoleteCode_cantChangeOwnEmail() throws Exception {
-//
-//		// credentials updated after the request for email change was made
-////		Thread.sleep(1L);
-//		AbstractUser<Long> user = getUserService().findById(getUser().getId()).get();
-//		user.setCredentialsUpdatedMillis(System.currentTimeMillis());
-//		userRepository.save(user);
-//
-////		Thread.sleep(1L);
-//
-//		// A new auth token is needed, because old one would be obsolete!
-//		String authToken = login2xx(UNVERIFIED_USER_EMAIL, UNVERIFIED_USER_PASSWORD);
-//
-//
-//		// now ready to test!
-//		mvc.perform(post(authProperties.getController().getChangeEmailUrl())
-//                .param("code", changeEmailCode)
-//				.param("id",getUser().getId().toString())
-//				.header(HttpHeaders.AUTHORIZATION, authToken)
-//                .header("contentType",  MediaType.APPLICATION_FORM_URLENCODED))
-//		        .andExpect(status().is(403));
-//	}
+	@Test
+	public void cantChangeOwnEmailWithObsoleteCode() throws Exception {
+		String token = login2xx(USER_EMAIL,USER_PASSWORD);
+		MailData mailData = testTemplate.requestEmailChange2xx(getUser().getId(), token,
+				new RequestEmailChangeDto(NEW_EMAIL));
+		// credentials updated after the request for email change was made
+//		Thread.sleep(1L);
+		AbstractUser<Long> user = getUserService().findById(getUser().getId()).get();
+		user.setCredentialsUpdatedMillis(System.currentTimeMillis());
+		getUserService().update(user);
+
+//		Thread.sleep(1L);
+
+		// A new auth token is needed, because old one would be obsolete!
+		token = login2xx(USER_EMAIL,USER_PASSWORD);
+
+
+		// now ready to test!
+		testTemplate.changeEmail(getUser(),getUser().getId(),mailData.getCode(),token)
+				//gets new token for new email to use
+				.andExpect(status().is(403));
+	}
 //
 //	/**
 //     * Trying without having requested first.
 //	 * @throws Exception
 //     */
-//	@Test
-//	public void didNotRequestEmailChange_cantChangeOwnEmail() throws Exception {
-//
-//		mvc.perform(post(authProperties.getController().getChangeEmailUrl(), getUser().getId())
-//                .param("code", changeEmailCode)
-//				.param("id",getUser().getId().toString())
-//				.header(HttpHeaders.AUTHORIZATION, tokens.get(getUser().getId()))
-//                .header("contentType",  MediaType.APPLICATION_FORM_URLENCODED))
-//		        .andExpect(status().is(400));
-//	}
+	@Test
+	public void cantChangeOwnEmailWithoutRequestingEmailChangeFirst() throws Exception {
+		String code = createChangeEmailToken(getUser(), NEW_EMAIL, 600000L);
+		String token = login2xx(USER_EMAIL,USER_PASSWORD);
+		testTemplate.changeEmail(getUser(),getUser().getId(),code,token)
+				//gets new token for new email to use
+				.andExpect(status().is(400));
+	}
 //
 //    /**
 //     * Trying after some user registers the newEmail, leaving it non unique.
 //     * @throws Exception
 //     */
-//	@Test
-//	public void newEmailNotUnique_cantChangeOwnEmail() throws Exception {
-//
-//		// Some other user changed to the same email
-//		AbstractUser<Long> user = getUserService().findById(getAdmin().getId()).get();
-//		user.setEmail(NEW_EMAIL);
-//		userRepository.save(user);
-//
-//		mvc.perform(post(authProperties.getController().getChangeEmailUrl(), getUser().getId())
-//                .param("code", changeEmailCode)
-//				.param("id",getUser().getId().toString())
-//				.header(HttpHeaders.AUTHORIZATION, tokens.get(getUser().getId()))
-//                .header("contentType",  MediaType.APPLICATION_FORM_URLENCODED))
-//		        .andExpect(status().is(400));
-//	}
+	@Test
+	public void cantChangeOwnEmailWhenNewEmailNotUnique() throws Exception {
 
+		String token = login2xx(USER_EMAIL,USER_PASSWORD);
+		MailData mailData = testTemplate.requestEmailChange2xx(getUser().getId(), token,
+				new RequestEmailChangeDto(NEW_EMAIL));
+
+		// Some other user changed to the same email, before i could issue my request
+		AbstractUser<Long> user = getUserService().findById(getAdmin().getId()).get();
+		user.setEmail(NEW_EMAIL);
+		getUserService().update(user);
+
+		testTemplate.changeEmail(getUser(),getUser().getId(),mailData.getCode(),token)
+				//gets new token for new email to use
+				.andExpect(status().is(400));
+	}
+
+	protected String createChangeEmailToken(AbstractUser targetUser, String newEmail, Long expiration){
+		return jweTokenService.createToken(
+				RapidJwt.create(
+						AbstractUserService.CHANGE_EMAIL_AUDIENCE,
+						targetUser.getId().toString(),
+						expiration,
+						MapUtils.mapOf("newEmail", newEmail)));
+	}
 
 }
