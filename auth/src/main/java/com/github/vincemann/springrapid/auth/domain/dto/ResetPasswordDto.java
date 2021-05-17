bb7504815e8f508336939a92f76654ed0947c53d
vincemann <v.conrad98@web.de>
2021-05-17 17:42:27 +0200

    Refactor ResetPasswordTest regarding #152.

--- auth/src/main/java/com/github/vincemann/springrapid/auth/domain/dto/ResetPasswordDto.java
+++ auth/src/main/java/com/github/vincemann/springrapid/auth/domain/dto/ResetPasswordDto.java
@@ -1,14 +1,13 @@
 package com.github.vincemann.springrapid.auth.domain.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
 
-@Getter @Setter @ToString
-public class ResetPasswordDto {
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor @Builder
+public class ResetPasswordDto implements Serializable {
 	
 	@NotBlank
 	@NotEmpty
