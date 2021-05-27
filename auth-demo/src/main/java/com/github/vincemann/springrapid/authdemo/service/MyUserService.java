fa7c7b96c6d0e1ced60e1290537ed08ec4679366
vincemann <v.conrad98@web.de>
2021-05-27 16:10:13 +0200

    Work on #161.

--- auth-demo/src/main/java/com/github/vincemann/springrapid/authdemo/service/MyUserService.java
+++ auth-demo/src/main/java/com/github/vincemann/springrapid/authdemo/service/MyUserService.java
@@ -1,8 +1,12 @@
 package com.github.vincemann.springrapid.authdemo.service;
 
+import com.github.vincemann.springrapid.auth.AuthProperties;
+import com.github.vincemann.springrapid.auth.domain.AuthRoles;
+import com.github.vincemann.springrapid.auth.service.AlreadyRegisteredException;
 import com.github.vincemann.springrapid.authdemo.model.User;
 import com.github.vincemann.springrapid.authdemo.repositories.UserRepository;
 import com.github.vincemann.springrapid.auth.service.AbstractUserService;
+import com.github.vincemann.springrapid.core.service.exception.BadEntityException;
 
 //@ServiceComponent
 //@Primary
@@ -23,4 +27,16 @@ public class MyUserService extends AbstractUserService<User, Long, UserRepositor
 		return MyUserService.class;
 	}
 
+	@Override
+	public User createAdminUser(AuthProperties.Admin admin) throws BadEntityException, AlreadyRegisteredException {
+		checkUniqueEmail(admin.getEmail());
+		checkValidPassword(admin.getPassword());
+		// create the user
+		User user = newUser();
+		user.setEmail(admin.getEmail());
+		user.setPassword(admin.getPassword());
+		user.getRoles().add(AuthRoles.ADMIN);
+		user.setName(admin.getEmail()+"name");
+		return save(user);
+	}
 }
