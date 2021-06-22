7bb757f183e81115db23c570072b77bc9d2717a3
vincemann <v.conrad98@web.de>
2021-06-22 20:16:11 +0200

    update to version 8

--- auth-tests/src/main/java/com/github/vincemann/springrapid/authtests/adapter/AuthTestAdapter.java
+++ auth-tests/src/main/java/com/github/vincemann/springrapid/authtests/adapter/AuthTestAdapter.java
@@ -5,34 +5,34 @@ import com.github.vincemann.springrapid.auth.dto.SignupDto;
 
 public abstract class AuthTestAdapter {
 
-    public static final String SIGNUP_USER_EMAIL = "signupUser@example.com";
-    public static final String SIGNUP_USER_PASSWORD = "signupUserSanjaySanjay99!";
-    public static final String NEW_EMAIL = "new.email@example.com";
-    public static final String NEW_PASSWORD = "newPasswordSanjaySanjay99!";
-    public static final String INVALID_EMAIL = "an-invalid-email";
-    public static final String INVALID_PASSWORD = "short";
-    public static final String UNKNOWN_EMAIL = "unknown@example.com";
+    public static  String SIGNUP_USER_EMAIL = "signupUser@example.com";
+    public static  String SIGNUP_USER_PASSWORD = "signupUserSanjaySanjay99!";
+    public static  String NEW_EMAIL = "new.email@example.com";
+    public static  String NEW_PASSWORD = "newPasswordSanjaySanjay99!";
+    public static  String INVALID_EMAIL = "an-invalid-email";
+    public static  String INVALID_PASSWORD = "short";
+    public static  String UNKNOWN_EMAIL = "unknown@example.com";
 
-    public static final String ADMIN_EMAIL = "admin@example.com";
-    public static final String ADMIN_PASSWORD = "adminSanjaySanjay99!";
+    public static  String ADMIN_EMAIL = "admin@example.com";
+    public static  String ADMIN_PASSWORD = "adminSanjaySanjay99!";
 
-    public static final String SECOND_ADMIN_EMAIL = "secondAdmin@example.com";
-    public static final String SECOND_ADMIN_PASSWORD = "secondAdminSanjaySanjay99!";
+    public static  String SECOND_ADMIN_EMAIL = "secondAdmin@example.com";
+    public static  String SECOND_ADMIN_PASSWORD = "secondAdminSanjaySanjay99!";
 
-    public static final String BLOCKED_ADMIN_EMAIL = "blockedAdmin@example.com";
-    public static final String BLOCKED_ADMIN_PASSWORD = "blockedAdminSanjaySanjay99!";
+    public static  String BLOCKED_ADMIN_EMAIL = "blockedAdmin@example.com";
+    public static  String BLOCKED_ADMIN_PASSWORD = "blockedAdminSanjaySanjay99!";
 
-    public static final String USER_EMAIL = "user@example.com";
-    public static final String USER_PASSWORD = "userSanjaySanjay99!";
+    public static  String USER_EMAIL = "user@example.com";
+    public static  String USER_PASSWORD = "userSanjaySanjay99!";
 
-    public static final String SECOND_USER_EMAIL = "secondUser@example.com";
-    public static final String SECOND_USER_PASSWORD = "secondUserSanjaySanjay99!";
+    public static  String SECOND_USER_EMAIL = "secondUser@example.com";
+    public static  String SECOND_USER_PASSWORD = "secondUserSanjaySanjay99!";
 
-    public static final String UNVERIFIED_USER_EMAIL = "unverifiedUser@example.com";
-    public static final String UNVERIFIED_USER_PASSWORD = "unverifiedUserSanjaySanjay99!";
+    public static  String UNVERIFIED_USER_EMAIL = "unverifiedUser@example.com";
+    public static  String UNVERIFIED_USER_PASSWORD = "unverifiedUserSanjaySanjay99!";
 
-    public static final String BLOCKED_USER_EMAIL = "blockedUser@example.com";
-    public static final String BLOCKED_USER_PASSWORD = "blockedUserSanjaySanjay99!";
+    public static  String BLOCKED_USER_EMAIL = "blockedUser@example.com";
+    public static  String BLOCKED_USER_PASSWORD = "blockedUserSanjaySanjay99!";
 
     public static String UNKNOWN_USER_ID = "99";
 
