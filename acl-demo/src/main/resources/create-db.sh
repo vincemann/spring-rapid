4c1b7a1e9cb7040bb543ac25ce8ed96183a9fbd3
vincemann <v.conrad98@web.de>
2021-12-13 21:30:50 +0100

    fix create db container scripts for demo projects

--- acl-demo/src/main/resources/create-db.sh
+++ acl-demo/src/main/resources/create-db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 homeDir=`cd ~ && pwd`
-rapidDir=$homeDir/.rapid
+rapidDir=$homeDir/.rapid/acl-db
+mkdir -p "$rapidDir"
 mkdir -p $rapidDir/mysql/data
 mkdir -p $rapidDir/mysql/conf.d
-sudo docker run --detach --name=lemon-mysql --env="MYSQL_ROOT_PASSWORD=mypassword" --env="MYSQL_DATABASE=aclDemoDb" --publish 6603:3306 --volume=$rapidDir/mysql/conf.d:/etc/mysql/conf.d --volume=$rapidDir/mysql/data:/var/lib/mysql mysql:8.0
+sudo docker run --detach --name=rapid-acl-mysql --env="MYSQL_ROOT_PASSWORD=mypassword" --env="MYSQL_DATABASE=aclDemoDb" --publish 6605:3306 --volume=$rapidDir/mysql/conf.d:/etc/mysql/conf.d --volume=$rapidDir/mysql/data:/var/lib/mysql mysql:8.0
 
--- auth-demo/src/main/resources/create-db.sh
+++ auth-demo/src/main/resources/create-db.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 homeDir=`cd ~ && pwd`
-rapidDir=$homeDir/.rapid
+rapidDir=$homeDir/.rapid/auth-db
+mkdir -p "$rapidDir"
 mkdir -p $rapidDir/mysql/data
 mkdir -p $rapidDir/mysql/conf.d
-sudo docker run --detach --name=lemon-mysql --env="MYSQL_ROOT_PASSWORD=mypassword" --env="MYSQL_DATABASE=authDemoDb" --publish 6603:3306 --volume=$rapidDir/mysql/conf.d:/etc/mysql/conf.d --volume=$rapidDir/mysql/data:/var/lib/mysql mysql:8.0
+sudo docker run --detach --name=rapid-auth-mysql --env="MYSQL_ROOT_PASSWORD=mypassword" --env="MYSQL_DATABASE=authDemoDb" --publish 6604:3306 --volume=$rapidDir/mysql/conf.d:/etc/mysql/conf.d --volume=$rapidDir/mysql/data:/var/lib/mysql mysql:8.0
 
