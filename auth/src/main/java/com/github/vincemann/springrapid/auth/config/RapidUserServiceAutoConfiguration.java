6affa8c830bf5d0f6942612dc707a3f212c726e9
vincemann <v.conrad98@web.de>
2022-05-18 23:18:23 +0200

    fix bug in UserUtils

--- auth/src/main/java/com/github/vincemann/springrapid/auth/config/RapidUserServiceAutoConfiguration.java
+++ auth/src/main/java/com/github/vincemann/springrapid/auth/config/RapidUserServiceAutoConfiguration.java
@@ -9,12 +9,14 @@ import com.github.vincemann.springrapid.core.model.RapidAuditorAware;
 import com.github.vincemann.springrapid.auth.service.RapidUserDetailsService;
 import com.github.vincemann.springrapid.auth.service.UserService;
 
+import com.github.vincemann.springrapid.core.service.locator.CrudServiceLocator;
 import com.github.vincemann.springrapid.core.service.password.BcryptRapidPasswordEncoder;
 import com.github.vincemann.springrapid.core.service.password.RapidPasswordEncoder;
 import com.github.vincemann.springrapid.core.slicing.ServiceConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.domain.AuditorAware;
@@ -79,9 +81,21 @@ public class RapidUserServiceAutoConfiguration {
         return new RapidPasswordValidator();
     }
 
-    @Autowired
-    public void configureAuthUtils(UserService<AbstractUser<Serializable>,Serializable> userService){
-        UserUtils.setUserService(userService);
+//    @Autowired
+//    public void configureAuthUtils(UserService<AbstractUser<Serializable>,Serializable> userService){
+//        UserUtils.setUserService(userService);
+//    }
+//    @Autowired
+//    public void configureAuthUtils(CrudServiceLocator crudServiceLocator, UserService<AbstractUser<Serializable>,Serializable> userService, ApplicationContext applicationContext){
+//        UserUtils.setCrudServiceLocator(crudServiceLocator);
+//        UserUtils.setUserService(userService);
+//        UserUtils.setApplicationContext(applicationContext);
+//    }
+
+    @Bean
+    @ConditionalOnMissingBean(UserUtils.class)
+    public UserUtils userUtils(){
+        return new UserUtils();
     }
 
 
