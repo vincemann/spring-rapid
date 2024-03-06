1e85cce0e37283223185e891051d06d9163ba7eb
vincemann <v.conrad98@web.de>
2024-03-06 13:31:44 +0100

    remove mysql example support from demo projects

--- auth-demo/src/main/resources/create-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-homeDir=`cd ~ && pwd`
-rapidDir=$homeDir/.rapid/auth-db
-mkdir -p "$rapidDir"
-mkdir -p $rapidDir/mysql/data
-mkdir -p $rapidDir/mysql/conf.d
-sudo docker run --detach --name=rapid-auth-mysql --env="MYSQL_ROOT_PASSWORD=mypassword" --env="MYSQL_DATABASE=authDemoDb" --publish 6604:3306 --volume=$rapidDir/mysql/conf.d:/etc/mysql/conf.d --volume=$rapidDir/mysql/data:/var/lib/mysql mysql:8.0
-
--- core-demo/src/main/resources/create-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-homeDir=`cd ~ && pwd`
-rapidDir=$homeDir/.rapid/core-db
-mkdir -p "$rapidDir"
-mkdir -p $rapidDir/mysql/data
-mkdir -p $rapidDir/mysql/conf.d
-sudo docker run --detach --name=rapid-core-mysql --env="MYSQL_ROOT_PASSWORD=mypassword" --env="MYSQL_DATABASE=coreDemoDb" --publish 6603:3306 --volume=$rapidDir/mysql/conf.d:/etc/mysql/conf.d --volume=$rapidDir/mysql/data:/var/lib/mysql mysql:8.0
-
