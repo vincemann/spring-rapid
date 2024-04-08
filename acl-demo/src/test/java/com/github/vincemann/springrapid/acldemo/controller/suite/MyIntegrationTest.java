ba7fd7b554b4194af0dcce3b0cf6f0243ddef25a
vincemann <v.conrad98@web.de>
2024-04-08 19:54:10 +0200

    minor changes

--- acl-demo/src/test/java/com/github/vincemann/springrapid/acldemo/controller/suite/MyIntegrationTest.java
+++ acl-demo/src/test/java/com/github/vincemann/springrapid/acldemo/controller/suite/MyIntegrationTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
